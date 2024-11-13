deb012ca24b72210d756fc855fcdfea4758c0fee
unsafe9 <realkorokke@outlook.com>
2024-11-13 20:12:42 +0900

    Change recovery just to wrap panic as an error

--- recovery.go
+++ recovery.go
@@ -2,23 +2,27 @@ package tinygrpc
 
 import (
 	"context"
-	"github.com/sirupsen/logrus"
+	"fmt"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 	"runtime/debug"
 )
+
+type PanicWrapper struct {
+	Panic any
+	Stack string
+}
+
+func (p PanicWrapper) Error() string {
+	return fmt.Sprintf("%v: %s", p.Panic, p.Stack)
+}
 
 func UnaryRecoveryInterceptor(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (_ any, retErr error) {
 	defer func() {
 		if r := recover(); r != nil {
-			logrus.WithFields(logrus.Fields{
-				"method": info.FullMethod,
-				"panic":  r,
-				"stack":  string(debug.Stack()),
-			}).Error()
-
-			retErr = status.Errorf(codes.Internal, "internal server error")
+			retErr = &PanicWrapper{
+				Panic: r,
+				Stack: string(debug.Stack()),
+			}
 		}
 	}()
 	return handler(ctx, req)
@@ -27,13 +31,10 @@ func UnaryRecoveryInterceptor(ctx context.Context, req any, info *grpc.UnaryServ
 func StreamRecoveryInterceptor(srv any, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) (retErr error) {
 	defer func() {
 		if r := recover(); r != nil {
-			logrus.WithFields(logrus.Fields{
-				"method": info.FullMethod,
-				"panic":  r,
-				"stack":  string(debug.Stack()),
-			}).Error()
-
-			retErr = status.Errorf(codes.Internal, "internal server error")
+			retErr = &PanicWrapper{
+				Panic: r,
+				Stack: string(debug.Stack()),
+			}
 		}
 	}()
 	return handler(srv, ss)
