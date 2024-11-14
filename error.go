f1d55a9ceaec7786daa5393b77cd94855e39532f
unsafe9 <realkorokke@outlook.com>
2024-11-14 19:06:13 +0900

    Add missing prefix

--- error.go
+++ error.go
@@ -7,14 +7,14 @@ import (
 
 type ErrorDeterminer func(c *CallContext, err error) error
 
-func UnaryErrorDeterminer(determiner ErrorDeterminer) grpc.UnaryServerInterceptor {
+func UnaryServerErrorDeterminer(determiner ErrorDeterminer) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
 		ret, err := handler(ctx, req)
 		return ret, determiner(newUnaryCallContext(ctx, info), err)
 	}
 }
 
-func StreamErrorDeterminer(determiner ErrorDeterminer) grpc.StreamServerInterceptor {
+func StreamServerErrorDeterminer(determiner ErrorDeterminer) grpc.StreamServerInterceptor {
 	return func(srv any, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
 		return determiner(newStreamCallContext(ss.Context(), srv, info), handler(srv, ss))
 	}
